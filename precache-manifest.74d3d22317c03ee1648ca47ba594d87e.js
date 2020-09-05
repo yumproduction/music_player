003c649d83471cad66133b3705423f8aa6b10ae3
max <salimova.mekhri@gmail.com>
2020-09-05 21:58:04 +0500

    Updates

---
+++ precache-manifest.74d3d22317c03ee1648ca47ba594d87e.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30da0228e965888a8bd6cfc320724ea5",
+    "url": "/music_player/index.html"
+  },
+  {
+    "revision": "2e0cd6b96f399d159ebc",
+    "url": "/music_player/static/css/2.aef21d24.chunk.css"
+  },
+  {
+    "revision": "e1eede6a1cb558c8b384",
+    "url": "/music_player/static/css/main.4b040dd0.chunk.css"
+  },
+  {
+    "revision": "2e0cd6b96f399d159ebc",
+    "url": "/music_player/static/js/2.dea2b1f9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/music_player/static/js/2.dea2b1f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1eede6a1cb558c8b384",
+    "url": "/music_player/static/js/main.3c3699ed.chunk.js"
+  },
+  {
+    "revision": "75697d2cb747193f5119",
+    "url": "/music_player/static/js/runtime-main.2fb2338e.js"
+  },
+  {
+    "revision": "be77eaec0c4b6564aa57bdff3e308312",
+    "url": "/music_player/static/media/Golden_Hour.be77eaec.mp3"
+  },
+  {
+    "revision": "24eeab3aa04a5728d6fb8098d834b122",
+    "url": "/music_player/static/media/Mad_Love.24eeab3a.mp3"
+  },
+  {
+    "revision": "d489253380e5ad2b20e03714a209e7a2",
+    "url": "/music_player/static/media/Starting_Again.d4892533.mp3"
+  },
+  {
+    "revision": "b2188350c4f0d90cd92be804750b7742",
+    "url": "/music_player/static/media/Sweetener.b2188350.mp3"
+  },
+  {
+    "revision": "2d031bbd01580e034269d1b4ef79e8c1",
+    "url": "/music_player/static/media/Youngblood.2d031bbd.mp3"
+  },
+  {
+    "revision": "6f1aca481d27eac99e510d153891e31e",
+    "url": "/music_player/static/media/fifth.6f1aca48.jpg"
+  },
+  {
+    "revision": "73269e61ca1221100e985892cde5c9e4",
+    "url": "/music_player/static/media/first.73269e61.jpg"
+  },
+  {
+    "revision": "414c3f4e59296c2175d31affcb10b6ec",
+    "url": "/music_player/static/media/forth.414c3f4e.jpg"
+  },
+  {
+    "revision": "e9b25b2e543e3d5e41e3d73f2c86c802",
+    "url": "/music_player/static/media/ic-next.e9b25b2e.svg"
+  },
+  {
+    "revision": "85288350c64101651bc85ecf9ec0df2e",
+    "url": "/music_player/static/media/ic-previous.85288350.svg"
+  },
+  {
+    "revision": "52e8b4b729061204e2190f00094e40c3",
+    "url": "/music_player/static/media/play-button.52e8b4b7.svg"
+  },
+  {
+    "revision": "cd4acaf05d7bea551f36b2d59cba7086",
+    "url": "/music_player/static/media/second.cd4acaf0.jpg"
+  },
+  {
+    "revision": "25b346a68ebdcc3238f04af10bc28161",
+    "url": "/music_player/static/media/stop-button.25b346a6.svg"
+  },
+  {
+    "revision": "d8b99c60b45a5f4a492b9a616ebe3926",
+    "url": "/music_player/static/media/third.d8b99c60.jpg"
+  }
+]);
